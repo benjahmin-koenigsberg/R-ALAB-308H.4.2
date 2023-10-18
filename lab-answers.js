dc505421a2d72f690747fccab8b3a2bf026c8ff8
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-10-18 11:38:41 -0400

    Easy Going answered

--- lab-answers.js
+++ lab-answers.js
@@ -1,6 +1,10 @@
 // Easy Going
 // Write a for loop that will log the numbers 1 through 20.
 
+for (let i = 1; i<20; i++){
+    console.log(i);
+}
+
 // The commit message should read: "Easy Going answered"
 
 
