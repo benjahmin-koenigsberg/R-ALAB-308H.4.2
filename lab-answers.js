ed22af7d9c206eca91015d164712d19a8bae7f4a
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-10-18 11:58:09 -0400

    Wild Wild Life answered

--- lab-answers.js
+++ lab-answers.js
@@ -46,15 +46,23 @@ if ( i % 5 === 0){
 
 // You should be modifying the elements by accessing them.It is up to you which methods to use.
 
-// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-// const sharky = ["Sharky", "shark", 20, "Left Coast"]
-// const plantee = ["Plantee", "plant", 5000, "Mordor"]
-// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-// const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant", 5000, "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
+
 // Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2] = 5001
+//console.log(plantee)
 // Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3] = "Gotham City";
+//console.log(wolfy)
 // Give D'Art a second hometown by adding "Hawkins"
+dart.push("Hawkins");
+console.log(dart)
 // Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+wolfy[0] = "Gameboy"
 // The commit message should read: "Wild Wild Life answered"
 
 
